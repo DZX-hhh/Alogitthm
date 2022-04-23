5bfa606d7584685e41a5ae5a2211768a2e14d539
daizixuan <2960895996@qq.com>
2022-04-23 11:19:45 +0800

    力扣杯--秋2019 T1:单调栈 T2:翻转+哈希表

---
+++ 力扣/力扣杯/秋2019/Day7/LCP1063.java
@@ -0,0 +1,33 @@
+package 力扣.力扣杯.秋2019.Day7;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class LCP1063 {
+
+    public int validSubarray2(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] >= nums[i]) res++;
+                else break;
+            }
+        }
+        return res;
+    }
+
+    public int validSubarrays(int[] nums) {
+        if (nums.length <= 1) return nums.length;
+        Deque<Integer> stack = new LinkedList<>();
+        int res = 0;
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() > num) {
+                stack.pop();
+            }
+            stack.push(num);
+            res += stack.size();//这里加size可以理解为当这个数之前的这些数字作为第一个的时候,,是可以的
+        }
+        return res;
+    }
+}
+
