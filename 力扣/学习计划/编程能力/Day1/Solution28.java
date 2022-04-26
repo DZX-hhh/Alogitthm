728880b2462b09c5a339a79d177c97545e139585
daizixuan <2960895996@qq.com>
2022-04-26 17:56:42 +0800

    编程能力--Day1 T1:单调队列:两个布尔值判断递增递减,如果出现nums[i]>nums[i-1]说明出现递增,,那么就必然不是单调递减队列,反之也是 T2:字符串匹配:KMP算法:主要是next[]的构造                        1:初始化next[0]=-1,,j=0,k=-1                        2:如果k==-1 || needle.charAt(j)==needle.charAt(k),,那么就开始构造next[++j]=++k                        3:否则,,k=need[k];让k跳到k的下一步,不断比较

---
+++ 力扣/学习计划/编程能力/Day1/Solution28.java
@@ -0,0 +1,52 @@
+package 力扣.学习计划.编程能力.Day1;
+
+public class Solution28 {
+    //直接调用库函数
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+    /**
+     * 字符串匹配问题,,KMP算法
+     */
+    public int strStr2(String haystack, String needle) {
+
+        //return haystack.indexOf(needle);
+
+        int[] next = new int[needle.length()];
+        getNext(needle, next);
+        int slow = 0, fast = 0;
+        while (slow < haystack.length() && fast < needle.length()) {
+            if ((fast == -1) || (haystack.charAt(slow) == needle.charAt(fast))) {
+                //如果匹配就继续遍历
+                fast++;
+                slow++;
+            } else {
+                //将fast较快的指针回退
+                fast = next[fast];
+            }
+        }
+
+        //如果fast成功走到needle的尾部,那么就用当前的slow-全部匹配成功的needle.length
+        if (fast >= needle.length()) {
+            return slow - needle.length();
+        }
+        return -1;//未找到
+    }
+
+    /**
+     * 构造next数组
+     */
+    public void getNext(String needle, int[] next) {
+        //初始化next[0]=-1
+        int i = 0, j = -1;
+        next[0] = -1;
+        while (i < needle.length() - 1) {
+            if (j == -1 || needle.charAt(i) == needle.charAt(j)) {
+                next[++i] = ++j;
+            } else {
+                j = next[j];
+            }
+        }
+    }
+}
