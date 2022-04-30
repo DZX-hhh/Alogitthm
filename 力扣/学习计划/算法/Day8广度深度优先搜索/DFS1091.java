bcfeace0d4ae36227a9f1a89382e1f16bc0e23f6
daizixuan <2960895996@qq.com>
2022-04-30 21:33:36 +0800

    算法--Day广度深度优先搜索 T1:网格图的最短路径:1.这里不能用dfs,因为dfs是尝试某一个路径,并直到结束,耗时间,更                           适合寻找某一个存在符合条件的路径                  2.这里适用bfs,因为bfs是全部同时进行,并且当找到某个符合条件的路径,这条路径就是最小值 T2:被围绕的区域:先将四周的'O'dfs变成'-',然后遍历将内部的'O'全部变成'X',并且将'-'变成'O'
    T3:所有可能的路径:可DFS也可BFS
                    决策树,,做出选择(onPath.add(From)),,递归(dfs From指向的所有节点)
                    撤销选择(onPath.removeLast() )

---
+++ 力扣/学习计划/算法/Day8广度深度优先搜索/DFS1091.java
@@ -0,0 +1,37 @@
+package 力扣.学习计划.算法.Day8广度深度优先搜索;
+
+public class DFS1091 {
+    int res = Integer.MAX_VALUE, step = 0;
+
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        dfs(grid, step, 0, 0);
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+
+    private void dfs(int[][] grid, int step, int r, int c) {
+        int n = grid.length;
+        //跳出边界
+        if (r < 0 || c < 0 || r >= n || c >= n) {
+            return;
+        }
+        //访问过或者无法访问
+        if (grid[r][c] != 0) {
+            return;
+        }
+        //记录访问路径
+        step++;
+        grid[r][c] = 2;
+        //到达顶点
+        if (r == n - 1 && c == n - 1) {
+            res = Math.min(res, step);//更新答案
+        }
+        //行列变换的8个方向
+        int[] rowDir = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] colDir = {-1, 0, 1, -1, 1, -1, 0, 1};
+        for (int i = 0; i < 8; i++) {
+            dfs(grid, step, r + rowDir[i], c + colDir[i]);
+        }
+        step--;
+        grid[r][c] = 0;
+    }
+}
---
+++ 力扣/学习计划/算法/Day8广度深度优先搜索/Solution797.java
@@ -0,0 +1,27 @@
+package 力扣.学习计划.算法.Day8广度深度优先搜索;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution797 {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        LinkedList<Integer> onPath = new LinkedList<>();
+        dfs(graph, res, onPath, 0);
+        return res;
+    }
+
+    private void dfs(int[][] graph, LinkedList<List<Integer>> res, LinkedList<Integer> onPath, int From) {
+        int n = graph.length;
+        onPath.addLast(From);
+        if (From == n - 1) {
+            res.add(new LinkedList<>(onPath));
+            onPath.removeLast();
+            return;
+        }
+        for (int To : graph[From]) {
+            dfs(graph, res, onPath, To);
+        }
+        onPath.removeLast();
+    }
+}
