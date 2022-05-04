6e06b8bb484e3f5534547a5552086913ab928bff
daizixuan <2960895996@qq.com>
2022-05-04 11:47:00 +0800

    算法--Day11递归回溯 T1:电话号码的字母组合         1.先String[]记录数字和字母的对应关系         2.start与同一集合的组合不同,这里代表的是第几个数字         3.再在数字对应的字符串内遍历,dfs下一个集合(dfs(start+1))
    T2:括号生成:由于需要匹配n个"(" 和 n个")" ,因此dfs+剪枝
               剪枝条件:当)出现的次数超过(的时候,,这个时候无法再挽回,直接return
                     dfs(n,leftCount+1,rightCount)
                     dfs(n,leftCount,rightCount+1)
               直到leftCount==rightCount==n代表结束(也可以逆向减到0)
    
    T3:单词搜索: dfs剪枝+回溯
                当前行列位置开始匹配,如果越界或者不匹配返回false
                                  如果idx==word.length-1,说明全部匹配,返回true
                否则dfs(r+-1,c+-1,idx+1)
                剪枝条件:访问过就将[r][c]置为'\0'
                撤销的时候再返回word[idx]

---
+++ 力扣/学习计划/算法/Day11递归回溯/Solution22.java
@@ -0,0 +1,49 @@
+package 力扣.学习计划.算法.Day11递归回溯;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution22 {
+
+    List<String> res = new LinkedList<>();
+
+    public List<String> generateParenthesis(int n) {
+        dfs(n, "", 0, 0);
+        return res;
+    }
+
+    /*
+        这里n对-->n个'(' 和 n个')'
+        dfs+剪枝
+        当'(' 数目< ')'的时候不再考虑
+     */
+    private void dfs(int n, String s, int leftCount, int rightCount) {
+        //剪枝
+        if (leftCount > n || rightCount > n || rightCount > leftCount) {
+            return;
+        }
+        if (leftCount == n && rightCount == n) {
+            res.add(s);
+            return;
+        }
+        dfs(n, s + '(', leftCount + 1, rightCount);
+        dfs(n, s + ')', leftCount, rightCount + 1);
+    }
+
+    public List<String> generateParenthesis2(int n) {
+        dfs2("", n, n);
+        return res;
+    }
+
+    private void dfs2(String s, int leftCount, int rightCount) {
+        if (leftCount < 0 || rightCount < 0 || rightCount < leftCount) {
+            return;
+        }
+        if (leftCount == 0 && rightCount == 0) {
+            res.add(s);
+            return;
+        }
+        dfs2(s + '(', leftCount - 1, rightCount);
+        dfs2(s + ')', leftCount, rightCount - 1);
+    }
+}
---
+++ 力扣/学习计划/算法/Day11递归回溯/Solution79.java
@@ -0,0 +1,51 @@
+package 力扣.学习计划.算法.Day11递归回溯;
+
+public class Solution79 {
+    public boolean exist(char[][] board, String word) {
+        char[] wordArr = word.toCharArray();
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                if (dfs(board, wordArr, 0, r, c)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param idx 字符当前索引
+     * @param r   当前行
+     * @param c   当前列
+     */
+    private boolean dfs(char[][] board, char[] wordArr, int idx, int r, int c) {
+        //1.递归终止条件,,超出边界
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length) {
+            return false;
+        }
+        //2.当前位置和字符数组不同,,就直接返回
+        if (board[r][c] != wordArr[idx]) {
+            return false;
+        }
+        //3.如果访问到了最后,返回true
+        if (idx == wordArr.length - 1) {
+            return true;
+        }
+
+        /*4.开始做出选择,让当前位置为空'\0'*/
+        board[r][c] = '\0';
+
+        //5开始递归dfs,,只要当前有一个选择可行,那就直接返回true
+        if (dfs(board, wordArr, idx + 1, r, c + 1) ||
+                dfs(board, wordArr, idx + 1, r, c - 1) ||
+                dfs(board, wordArr, idx + 1, r + 1, c) ||
+                dfs(board, wordArr, idx + 1, r - 1, c)
+        ) {
+            return true;
+        }
+        //5.撤销选择
+        board[r][c] = wordArr[idx];
+
+        return false;
+    }
+}
