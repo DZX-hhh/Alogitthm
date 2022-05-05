ceff3fa9f39b3f0713b91bd551ba99ed4acd2cfc
daizixuan <2960895996@qq.com>
2022-05-05 17:13:20 +0800

    剑指Offer-Day12动态规划 T1:打家劫舍1:线性房屋            递推式: dp[i]=Math.max(dp[i-1],(dp[i-2]+nums[i]))            含义:当天最大值==max(当天偷的+前天偷的最大值,昨天偷的最大值)
      打家劫舍2:环形房屋
               递推式:同上
               解决方法:将数组划分成两个数组
                       * 一个忽略头部,,一个忽略尾部
    
      打家劫舍3:树形房屋
                 1.暴力递归:超时,存在过多左右子树重复计算
                   考虑父节点偷与不偷
                   > 父节点偷  -->左右孩子不能偷(val+f(root.l.l)+f(root.l.r)+...)
                   >父节点不偷 -->考虑左右孩子偷与不偷(递归f(root.left)+f(root.right))
                   返回max(偷,不偷)
    
                 2.考虑到方法1存在过多重复计算-->哈希表记录,记忆化搜索
                    map<root,maxVal>
                    >递归过程中map有记录,则直接返回记录
                    >递归过程中map没有记录,计算出来结果并map.put(root,maxVal)
    
                 3.树形dp[]
                    - dp长度为2
                            > dp[0]含义是不偷当前节点的最值
                            > dp[1]含义是偷当前节点的最值
    
                    - 这里使用后序遍历,,先得出robTree(root.left),robTree(root.right)
                            > dp[1]=root.val+left[0]+right[0]
                            > dp[0]=max(left[0],left[1])+max(right[0],right[1])
                            return max(dp[0],dp[1])
    
    T2:跳跃游戏
             - 贪心:维护数组能够到达的最大距离maxDistance
                   遍历数组,判断maxDistance是否覆盖当前位置
                           > (如果不能,那么说明无法到达此位置,返回false)
                           > 如果能,更新max(maxDistance,i+num[i])
      跳跃游戏2
              -贪心:维护当前覆盖范围nowDistance以及下一步覆盖范围nextDistance
                               > 当下标i到达当前覆盖范围nowDistance
                                      - 如果当前下标为数组末尾,直接返回步数
                                      - 不是尾部,步数+1,更新当前覆盖范围为下一步覆盖范围
                               > 当前下标没到达,continue
    
              -改进版:如果nextDistance>=length-1   return step+1
                     如果i到达当前覆盖范围尾部,更新当前最大覆盖范围,step++

---
+++ 力扣/学习计划/算法/Day12动态规划/Solution198.java
@@ -0,0 +1,23 @@
+package 力扣.学习计划.算法.Day12动态规划;
+
+public class Solution198 {
+    /**
+     * 递推式: dp[i]=Math.max(dp[i-1],(dp[i-2]+nums[i]))
+     * 含义:当天最大值==max(当天偷的+前天偷的最大值,昨天偷的最大值)
+     */
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[dp.length - 1];
+    }
+}
---
+++ 力扣/学习计划/算法/Day12动态规划/Solution55.java
@@ -0,0 +1,22 @@
+package 力扣.学习计划.算法.Day12动态规划;
+
+public class Solution55 {
+    /*
+        贪心:维护能够到达的最远距离
+        遍历数组,判断最远距离是否能够覆盖当前位置,并比较最远距离和i+nums[i]
+     */
+    public boolean canJump(int[] nums) {
+        if (nums.length <= 1) {
+            return true;
+        }
+        int maxDistance = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (maxDistance >= i) {//能够到达
+                maxDistance = Math.max(maxDistance, i + nums[i]);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
