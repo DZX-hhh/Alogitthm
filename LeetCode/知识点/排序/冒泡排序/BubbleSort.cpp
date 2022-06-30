036b335afba2d99fbe8ab9980cd895584bad4370
daizixuan <2960895996@qq.com>
2022-07-01 00:00:39 +0800

    冒泡排序(稳定):1.进行n-1次冒泡:每一次冒泡的结果可以是         1.从后往前...每一趟冒泡使得最小的元素到前面         2.从前到后,,每一趟冒泡使得最大的值到后面

---
+++ LeetCode/知识点/排序/冒泡排序/BubbleSort.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* 将最小的数冒泡到最前 */
+void BubbleSort_Small(int A[], int n) {
+	for (int i = 0; i < n - 1; i++) {//需要冒泡n-1个数,,也就是n-1次
+		bool flag = false;
+		for (int j = n - 1; j >= i; j--) {//将最小的数冒泡到最前
+			if (A[j] < A[j - 1]) {
+				swap(A[j], A[j - 1]);//交换
+				flag = true;
+			}
+		}
+		//发生冒泡的标记,,如果为false,,那么说明已经有序
+		if (!flag) {
+			return;
+		}
+	}
+}
+/* 最大的数冒泡到最后 */
+void BubbleSort_Big(int A[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		bool flag = false;
+		for (int j = 0; j < n - i - 1; j++) {//这里冒泡将最大的数冒泡到最后
+			if (A[j] < A[j + 1]) {
+				swap(A[j], A[j + 1]);
+				flag = true;
+			}
+		}
+		if (!flag) {
+			return;
+		}
+	}
+}
+
+/* 交换 */
+void swap(int& a, int& b) {
+	int t = a;
+	a = b;
+	b = t;
+}
