312f11597361f6d2c783b7eaf26e26c4d56941a1
dzx0217 <dzx0217@qq.com>
2024-09-14 23:07:27 +0800

    LeetcodeGame139

---
+++ Leetcode/周赛/Game139/source.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> stableMountains(vector<int>& height, int threshold) {
+    vector<int> res;
+    int max = height[0];
+    for (int i = 1; i < height.size(); i++) {
+      if (height[i - 1] > threshold) res.push_back(i);
+    }
+    return res;
+  }
+};
